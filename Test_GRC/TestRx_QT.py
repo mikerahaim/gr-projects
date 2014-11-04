ef67db46b73718701c1efc9b452640f39c40b194
mikerahaim <rahaimm@gmail.com>
2014-11-04 12:55:56 -0500

    Added test GRC files (using both WX GUI and QT GUI). These files display input from the USRP.

---
+++ Test_GRC/TestRx_QT.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: QT GUI signal reader (from USRP)
+# Author: MR
+# Description: Read input from USRP and display output with and without filtering
+# Generated: Tue Nov  4 12:52:23 2014
+##################################################
+
+from PyQt4 import Qt
+from PyQt4.QtCore import QObject, pyqtSlot
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import PyQt4.Qwt5 as Qwt
+import sip
+import sys
+import time
+
+from distutils.version import StrictVersion
+class TestRx_QT(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "QT GUI signal reader (from USRP)")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("QT GUI signal reader (from USRP)")
+        try:
+             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+             pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "TestRx_QT")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2000000
+        self.trans_width = trans_width = 10000
+        self.gain = gain = 1
+        self.co_freq = co_freq = samp_rate/2
+        self.car_freq = car_freq = 1000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._trans_width_layout = Qt.QHBoxLayout()
+        self._trans_width_layout.addWidget(Qt.QLabel("Transition Width"+": "))
+        class qwt_counter_pyslot(Qwt.QwtCounter):
+            def __init__(self, parent=None):
+                Qwt.QwtCounter.__init__(self, parent)
+            @pyqtSlot('double')
+            def setValue(self, value):
+                super(Qwt.QwtCounter, self).setValue(value)
+        self._trans_width_counter = qwt_counter_pyslot()
+        self._trans_width_counter.setRange(1000, 100000, 1000)
+        self._trans_width_counter.setNumButtons(2)
+        self._trans_width_counter.setMinimumWidth(200)
+        self._trans_width_counter.setValue(self.trans_width)
+        self._trans_width_layout.addWidget(self._trans_width_counter)
+        self._trans_width_counter.valueChanged.connect(self.set_trans_width)
+        self.top_grid_layout.addLayout(self._trans_width_layout, 4,1,1,1)
+        self.tab_widget = Qt.QTabWidget()
+        self.tab_widget_widget_0 = Qt.QWidget()
+        self.tab_widget_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.tab_widget_widget_0)
+        self.tab_widget_grid_layout_0 = Qt.QGridLayout()
+        self.tab_widget_layout_0.addLayout(self.tab_widget_grid_layout_0)
+        self.tab_widget.addTab(self.tab_widget_widget_0, "Unfiltered")
+        self.tab_widget_widget_1 = Qt.QWidget()
+        self.tab_widget_layout_1 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.tab_widget_widget_1)
+        self.tab_widget_grid_layout_1 = Qt.QGridLayout()
+        self.tab_widget_layout_1.addLayout(self.tab_widget_grid_layout_1)
+        self.tab_widget.addTab(self.tab_widget_widget_1, "Filtered")
+        self.top_grid_layout.addWidget(self.tab_widget, 1,1,1,1)
+        self._gain_layout = Qt.QHBoxLayout()
+        self._gain_layout.addWidget(Qt.QLabel("Filter Gain"+": "))
+        class qwt_counter_pyslot(Qwt.QwtCounter):
+            def __init__(self, parent=None):
+                Qwt.QwtCounter.__init__(self, parent)
+            @pyqtSlot('double')
+            def setValue(self, value):
+                super(Qwt.QwtCounter, self).setValue(value)
+        self._gain_counter = qwt_counter_pyslot()
+        self._gain_counter.setRange(1, 10, 1)
+        self._gain_counter.setNumButtons(2)
+        self._gain_counter.setMinimumWidth(200)
+        self._gain_counter.setValue(self.gain)
+        self._gain_layout.addWidget(self._gain_counter)
+        self._gain_counter.valueChanged.connect(self.set_gain)
+        self.top_grid_layout.addLayout(self._gain_layout, 5,1,1,1)
+        self._co_freq_layout = Qt.QHBoxLayout()
+        self._co_freq_layout.addWidget(Qt.QLabel("Cutoff Frequency"+": "))
+        class qwt_counter_pyslot(Qwt.QwtCounter):
+            def __init__(self, parent=None):
+                Qwt.QwtCounter.__init__(self, parent)
+            @pyqtSlot('double')
+            def setValue(self, value):
+                super(Qwt.QwtCounter, self).setValue(value)
+        self._co_freq_counter = qwt_counter_pyslot()
+        self._co_freq_counter.setRange(0, samp_rate/2, 100000)
+        self._co_freq_counter.setNumButtons(2)
+        self._co_freq_counter.setMinimumWidth(200)
+        self._co_freq_counter.setValue(self.co_freq)
+        self._co_freq_layout.addWidget(self._co_freq_counter)
+        self._co_freq_counter.valueChanged.connect(self.set_co_freq)
+        self.top_grid_layout.addLayout(self._co_freq_layout, 3,1,1,1)
+        self._car_freq_layout = Qt.QHBoxLayout()
+        self._car_freq_layout.addWidget(Qt.QLabel("Carrier Frequency"+": "))
+        class qwt_counter_pyslot(Qwt.QwtCounter):
+            def __init__(self, parent=None):
+                Qwt.QwtCounter.__init__(self, parent)
+            @pyqtSlot('double')
+            def setValue(self, value):
+                super(Qwt.QwtCounter, self).setValue(value)
+        self._car_freq_counter = qwt_counter_pyslot()
+        self._car_freq_counter.setRange(0, 5000000, 100000)
+        self._car_freq_counter.setNumButtons(2)
+        self._car_freq_counter.setMinimumWidth(200)
+        self._car_freq_counter.setValue(self.car_freq)
+        self._car_freq_layout.addWidget(self._car_freq_counter)
+        self._car_freq_counter.valueChanged.connect(self.set_car_freq)
+        self.top_grid_layout.addLayout(self._car_freq_layout, 2,1,1,1)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(car_freq, 0)
+        self.uhd_usrp_source_0.set_gain(0, 0)
+        self.qtgui_sink_x_0_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"QT GUI Plot", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.tab_widget_layout_1.addWidget(self._qtgui_sink_x_0_0_win)
+        
+        self.qtgui_sink_x_0_0.enable_rf_freq(False)
+        
+        
+          
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"QT GUI Plot", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.tab_widget_layout_0.addWidget(self._qtgui_sink_x_0_win)
+        
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+        
+        
+          
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	gain, samp_rate, co_freq, trans_width, firdes.WIN_HAMMING, 6.76))
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_sink_x_0_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.low_pass_filter_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "TestRx_QT")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_sink_x_0_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.co_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
+        self.set_co_freq(self.samp_rate/2)
+
+    def get_trans_width(self):
+        return self.trans_width
+
+    def set_trans_width(self, trans_width):
+        self.trans_width = trans_width
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.co_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
+        Qt.QMetaObject.invokeMethod(self._trans_width_counter, "setValue", Qt.Q_ARG("double", self.trans_width))
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.co_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
+        Qt.QMetaObject.invokeMethod(self._gain_counter, "setValue", Qt.Q_ARG("double", self.gain))
+
+    def get_co_freq(self):
+        return self.co_freq
+
+    def set_co_freq(self, co_freq):
+        self.co_freq = co_freq
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.gain, self.samp_rate, self.co_freq, self.trans_width, firdes.WIN_HAMMING, 6.76))
+        Qt.QMetaObject.invokeMethod(self._co_freq_counter, "setValue", Qt.Q_ARG("double", self.co_freq))
+
+    def get_car_freq(self):
+        return self.car_freq
+
+    def set_car_freq(self, car_freq):
+        self.car_freq = car_freq
+        self.uhd_usrp_source_0.set_center_freq(self.car_freq, 0)
+        Qt.QMetaObject.invokeMethod(self._car_freq_counter, "setValue", Qt.Q_ARG("double", self.car_freq))
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    if(StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0")):
+        Qt.QApplication.setGraphicsSystem(gr.prefs().get_string('qtgui','style','raster'))
+    qapp = Qt.QApplication(sys.argv)
+    tb = TestRx_QT()
+    tb.start()
+    tb.show()
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+    tb = None #to clean up Qt widgets
---
+++ Test_GRC/TestRx_WX.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: WX GUI signal reader (from USRP)
+# Author: MR
+# Description: Read input from USRP and display output with and without filtering
+# Generated: Tue Nov  4 12:52:51 2014
+##################################################
+
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import time
+import wx
+
+class TestRx_WX(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="WX GUI signal reader (from USRP)")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2000000
+        self.filter_transition = filter_transition = 10000
+        self.filter_gain = filter_gain = 1
+        self.co_freq = co_freq = samp_rate/2
+        self.car_freq = car_freq = 0
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.notebook_0 = self.notebook_0 = wx.Notebook(self.GetWin(), style=wx.NB_TOP)
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Signal")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Freq")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Signal (Filtered)")
+        self.notebook_0.AddPage(grc_wxgui.Panel(self.notebook_0), "Freq (Filtered)")
+        self.GridAdd(self.notebook_0, 0, 0, 1, 4)
+        _filter_transition_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._filter_transition_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_filter_transition_sizer,
+        	value=self.filter_transition,
+        	callback=self.set_filter_transition,
+        	label="Filter Transition Width",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._filter_transition_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_filter_transition_sizer,
+        	value=self.filter_transition,
+        	callback=self.set_filter_transition,
+        	minimum=1000,
+        	maximum=100000,
+        	num_steps=99,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_filter_transition_sizer, 2, 0, 1, 2)
+        _filter_gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._filter_gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_filter_gain_sizer,
+        	value=self.filter_gain,
+        	callback=self.set_filter_gain,
+        	label="Filter Gain",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._filter_gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_filter_gain_sizer,
+        	value=self.filter_gain,
+        	callback=self.set_filter_gain,
+        	minimum=1,
+        	maximum=32,
+        	num_steps=31,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_filter_gain_sizer, 2, 2, 1, 2)
+        _co_freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._co_freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_co_freq_sizer,
+        	value=self.co_freq,
+        	callback=self.set_co_freq,
+        	label="Filter Cutoff Frequency",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._co_freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_co_freq_sizer,
+        	value=self.co_freq,
+        	callback=self.set_co_freq,
+        	minimum=0,
+        	maximum=samp_rate/2,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_co_freq_sizer, 1, 0, 1, 2)
+        _car_freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._car_freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_car_freq_sizer,
+        	value=self.car_freq,
+        	callback=self.set_car_freq,
+        	label="Carrier Frequency",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._car_freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_car_freq_sizer,
+        	value=self.car_freq,
+        	callback=self.set_car_freq,
+        	minimum=0,
+        	maximum=5000000,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.GridAdd(_car_freq_sizer, 1, 2, 1, 2)
+        self.wxgui_scopesink2_0_0 = scopesink2.scope_sink_c(
+        	self.notebook_0.GetPage(2).GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.notebook_0.GetPage(2).Add(self.wxgui_scopesink2_0_0.win)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_c(
+        	self.notebook_0.GetPage(0).GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.notebook_0.GetPage(0).Add(self.wxgui_scopesink2_0.win)
+        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
+        	self.notebook_0.GetPage(3).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=5,
+        	y_divs=10,
+        	ref_level=-80,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(3).Add(self.wxgui_fftsink2_0_0.win)
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.notebook_0.GetPage(1).GetWin(),
+        	baseband_freq=0,
+        	y_per_div=5,
+        	y_divs=10,
+        	ref_level=-80,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.notebook_0.GetPage(1).Add(self.wxgui_fftsink2_0.win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(("", "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_source_0.set_center_freq(car_freq, 0)
+        self.uhd_usrp_source_0.set_gain(0, 0)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	filter_gain, samp_rate, co_freq, filter_transition, firdes.WIN_HAMMING, 6.76))
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_scopesink2_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.wxgui_fftsink2_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.wxgui_fftsink2_0_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.wxgui_scopesink2_0_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.low_pass_filter_0, 0))
+
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.filter_gain, self.samp_rate, self.co_freq, self.filter_transition, firdes.WIN_HAMMING, 6.76))
+        self.set_co_freq(self.samp_rate/2)
+
+    def get_filter_transition(self):
+        return self.filter_transition
+
+    def set_filter_transition(self, filter_transition):
+        self.filter_transition = filter_transition
+        self._filter_transition_slider.set_value(self.filter_transition)
+        self._filter_transition_text_box.set_value(self.filter_transition)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.filter_gain, self.samp_rate, self.co_freq, self.filter_transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_filter_gain(self):
+        return self.filter_gain
+
+    def set_filter_gain(self, filter_gain):
+        self.filter_gain = filter_gain
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.filter_gain, self.samp_rate, self.co_freq, self.filter_transition, firdes.WIN_HAMMING, 6.76))
+        self._filter_gain_slider.set_value(self.filter_gain)
+        self._filter_gain_text_box.set_value(self.filter_gain)
+
+    def get_co_freq(self):
+        return self.co_freq
+
+    def set_co_freq(self, co_freq):
+        self.co_freq = co_freq
+        self.low_pass_filter_0.set_taps(firdes.low_pass(self.filter_gain, self.samp_rate, self.co_freq, self.filter_transition, firdes.WIN_HAMMING, 6.76))
+        self._co_freq_slider.set_value(self.co_freq)
+        self._co_freq_text_box.set_value(self.co_freq)
+
+    def get_car_freq(self):
+        return self.car_freq
+
+    def set_car_freq(self, car_freq):
+        self.car_freq = car_freq
+        self.uhd_usrp_source_0.set_center_freq(self.car_freq, 0)
+        self._car_freq_slider.set_value(self.car_freq)
+        self._car_freq_text_box.set_value(self.car_freq)
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = TestRx_WX()
+    tb.Start(True)
+    tb.Wait()
